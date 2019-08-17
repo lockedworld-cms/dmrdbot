f93b5a92af5b5f1172a9ab2c504d7029b9f5b6e5
lockedworld-cms <48277019+lockedworld-cms@users.noreply.github.com>
2019-08-18 01:53:34 +0400

    Update config.js
    S

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ const config = {
   "token": ayarlar.token,
 
   "dashboard" : {
-    "oauthSecret": "XBx8HtswD6B-wmJAETvRx3vEYIJUepoF", // This is the `client` secret in your bot application page.
-    "callbackURL": "https://saitokontrol.glitch.me/callback",
+    "oauthSecret": "xAB9AskIGIJuzkEzaUuhbtc6aZrp9fSl", // This is the `client` secret in your bot application page.
+    "callbackURL": "https://lockedworld-xan.glitch.me/callback",
     "sessionSecret": "super-secret-session-thing",
-    "domain": "https://saitokontrol.glitch.me",
+    "domain": "https://lockedworld-xan.glitch.me",
     "port": 8000
   },
 };
